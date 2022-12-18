d8f98920d1bada7d8d20ce831b347b4dfb113063
macerocket <87500880+macerocket@users.noreply.github.com>
2022-12-18 15:26:54 +0000

    Update for day 1 answer

--- day1/day1.py
+++ day1/day1.py
@@ -2,14 +2,25 @@
 """
 Day 1
 """
-import pandas as pd
 
-def get_length(filename: str) -> int:
+def get_max_calories(filename: str) -> int:
     """
-    Get the length of the input
+    Get the elf with the maximum calries from the input file
     """
-    aoc_df = pd.read_csv(filename)
-    return len(aoc_df)
+    maximum = 0
+    accumulator = 0
+
+    data = open(filename, "r").readlines()
+
+    for line in data:
+        if line == "" :
+            if accumulator > maximum:
+                maximum = accumulator
+            accumulator = 0
+        else :
+            accumulator = accumulator + int(line)
+
+    return maximum
 
 if __name__ == "__main__":
-    get_length('day1/input.txt')
+    get_max_calories('day1/input.txt')
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -2,10 +2,16 @@
 Tests for day 1 AOC2022
 """
 
-from day1 import get_length
+from day1 import get_max_calories
 
-def test_get_length():
+def test_get_max_calories():
     """
-    Test for get_length function
+    Test for get_max_calories function
     """
-    assert get_length('day1/test_input.txt') == 5
+    assert get_max_calories('day1/test_input.txt') == 9
+
+def test_day1():
+    """
+    Test for getting the actual value for day 1
+    """
+    print(f"Answer for day 1: {get_max_calories('day1/input.txt')}")
