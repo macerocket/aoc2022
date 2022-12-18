1f61f83bf947cac74059dbf8befb145559c36b14
macerocket <87500880+macerocket@users.noreply.github.com>
2022-12-18 15:44:03 +0000

    try capturing sysout

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -2,7 +2,6 @@
 Tests for day 1 AOC2022
 """
 
-import logging
 from day1 import get_max_calories
 
 def test_get_max_calories():
@@ -12,9 +11,9 @@ def test_get_max_calories():
     assert get_max_calories('day1/test_input.txt') == 9
 
 
-def test_day1():
+def test_day1(capsys):
     """
     Test for getting the actual value for day 1
     """
-    log = logging.getLogger( "AOC2022.day1" )
-    log.debug("Answer for day 1: %s", {get_max_calories('day1/input.txt')})
+    with capsys.disabled():
+        print("Answer for day 1: %s", {get_max_calories('day1/input.txt')})
