40d807bb6077fd7d924cb2552e2cff17f569dcef
macerocket <87500880+macerocket@users.noreply.github.com>
2022-12-18 10:40:19 +0000

    fix lint errors

--- day1/day1.py
+++ day1/day1.py
@@ -1,8 +1,15 @@
+
+"""
+Day 1
+"""
 import pandas as pd
 
 def get_length(filename: str) -> int:
-  df = pd.read_csv(filename)
-  return len(df)
+    """
+    Get the length of the input
+    """
+    aoc_df = pd.read_csv(filename)
+    return len(aoc_df)
 
 if __name__ == "__main__":
-  get_length('input.txt')
+    get_length('day1/input.txt')
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -1,4 +1,11 @@
+"""
+Tests for day 1 AOC2022
+"""
+
 from day1 import get_length
 
 def test_get_length():
-  assert get_length('day1/test_input.txt') == 5
+    """
+    Test for get_length function
+    """
+    assert get_length('day1/test_input.txt') == 5
