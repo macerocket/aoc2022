1cadc4dde6d9d93fc4bea2c1117e25952c002529
macerocket <87500880+macerocket@users.noreply.github.com>
2022-12-18 15:41:11 +0000

    Try logging the output instead of printing

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -2,6 +2,7 @@
 Tests for day 1 AOC2022
 """
 
+import logging
 from day1 import get_max_calories
 
 def test_get_max_calories():
@@ -10,8 +11,10 @@ def test_get_max_calories():
     """
     assert get_max_calories('day1/test_input.txt') == 9
 
+
 def test_day1():
     """
     Test for getting the actual value for day 1
     """
-    print(f"Answer for day 1: {get_max_calories('day1/input.txt')}")
+    log = logging.getLogger( "AOC2022.day1" )
+    log.debug("Answer for day 1: %s", {get_max_calories('day1/input.txt')})
