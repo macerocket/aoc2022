8eebe9d0806f8be361faf278dcee981bdd0d4a3e
macerocket <87500880+macerocket@users.noreply.github.com>
2022-12-22 22:05:52 +0000

    Day 2 part 2 - DONE :tada:

--- day2/day2.py
+++ day2/day2.py
@@ -39,27 +39,27 @@ def get_winner(opponent_play: str, my_play: str) -> str:
     Calculate who wins a game of roshambo
     """
     if opponent_play == OPPONENT_ROCK:
-        if my_play == PAPER: 
+        if my_play == PAPER:
             return WIN
-        if my_play == ROCK: 
+        if my_play == ROCK:
             return DRAW
     if opponent_play == OPPONENT_PAPER:
-        if my_play == SCISSORS: 
+        if my_play == SCISSORS:
             return WIN
-        if my_play == PAPER: 
+        if my_play == PAPER:
             return DRAW
     if opponent_play == OPPONENT_SCISSORS:
-        if my_play == ROCK: 
+        if my_play == ROCK:
             return WIN
-        if my_play == SCISSORS: 
+        if my_play == SCISSORS:
             return DRAW
     return LOSE
 
 def calculate_score(outcome: str, played: str) -> int:
     """
-    (1 for Rock, 2 for Paper, and 3 for Scissors) 
-        plus the score for the outcome of the round 
-    (0 if you lost, 3 if the round was a draw, and 6 if you won).
+    (1 for Rock, 2 for Paper, and 3 for Scissors)
+        plus the score for the outcome of the round
+    (0 if you lost, 3 if the round was a draw, and 6 if you won)
 
     """
     score = 0
@@ -67,11 +67,11 @@ def calculate_score(outcome: str, played: str) -> int:
         score += 6
     if outcome == DRAW:
         score += 3
-    if played == ROCK:
+    if played == ROCK or played == OPPONENT_ROCK:
         score += 1
-    if played == PAPER:
+    if played == PAPER or played == OPPONENT_PAPER:
         score += 2
-    if played == SCISSORS:
+    if played == SCISSORS or played == OPPONENT_SCISSORS:
         score += 3
     return score
 
@@ -94,4 +94,60 @@ def get_total_score(filename: str) -> int:
     return grand_total
 
 
+def get_correct_play(opponent_play: str, required_outcome: str) -> str:
+    """
+    Calculate what to play to acheive the desired outcome of a game of roshambo
+    X means you need to lose, 
+    Y means you need to end the round in a draw, and 
+    Z means you need to win.
+
+    """
+    WIN = "Z"
+    LOSE = "X"
+    DRAW = "Y"
+    if opponent_play == OPPONENT_ROCK:
+        if required_outcome == WIN:
+            return PAPER
+        if required_outcome == DRAW:
+            return ROCK
+        if required_outcome == LOSE:
+            return SCISSORS
+    if opponent_play == OPPONENT_PAPER:
+        if required_outcome == WIN:
+            return SCISSORS
+        if required_outcome == DRAW:
+            return PAPER
+        if required_outcome == LOSE:
+            return ROCK
+    if opponent_play == OPPONENT_SCISSORS:
+        if required_outcome == WIN:
+            return ROCK
+        if required_outcome == DRAW:
+            return SCISSORS
+        if required_outcome == LOSE:
+            return PAPER
+
+
+def get_total_score_part2(filename: str) -> int:
+    """
+    For each line of the input, we need to determine the correct play
+    then calculate the score and add it to the running total
+    returning the grand total at the end
+    """
+    data = open(filename, "r").readlines()
+    data = [x.strip() for x in data]
+
+    grand_total:int = 0
+
+    for line in data:
+        (opponent_played, outcome) = line.split(" ")
+        if outcome == "X": required_outcome = LOSE
+        if outcome == "Y": required_outcome = DRAW
+        if outcome == "Z": required_outcome = WIN
+        
+        correct_play:str = get_correct_play(opponent_played, outcome)
+        grand_total += calculate_score(required_outcome, correct_play)
+        print(f"score for {opponent_played}, {required_outcome} (correct play is {correct_play}) is {calculate_score(required_outcome, correct_play)}")
+
+    return grand_total
 
--- day2/test_day2.py
+++ day2/test_day2.py
@@ -3,7 +3,7 @@ Tests for day 2 AOC2022
 """
 
 from day2 import WIN, LOSE, DRAW, ROCK, PAPER, SCISSORS, get_winner, \
-    calculate_score, get_total_score
+    calculate_score, get_total_score, get_correct_play, get_total_score_part2
 
 def test_get_winner():
     """
@@ -17,9 +17,24 @@ def test_calculate_score():
     """
     Test for calculate_score function
     """
-    assert(calculate_score(WIN, ROCK) == 7)
-    assert(calculate_score(DRAW, PAPER) == 5)
-    assert(calculate_score(LOSE, SCISSORS) == 3)
+    assert calculate_score(WIN, ROCK) == 7
+    assert calculate_score(DRAW, PAPER) == 5
+    assert calculate_score(LOSE, SCISSORS) == 3
+
+def test_get_correct_play():
+    """
+    Test the get_correct_play function
+    A for Rock, B for Paper, and C for Scissors
+    X for Rock, Y for Paper, and Z for Scissors
+
+    X means you need to lose, 
+    Y means you need to end the round in a draw, and 
+    Z means you need to win
+    """
+    assert get_correct_play("A", "X") == "Z"
+    assert get_correct_play("A", "Z") == "Y"
+    assert get_correct_play("B", "Z") == "Z"
+    assert get_correct_play("C", "X") == "Y"
 
 def test_get_summary():
     """
@@ -33,3 +48,11 @@ def test_day2_part1(capsys):
     """
     with capsys.disabled():
         print(f"Answer for day 2 part1: {get_total_score('day2/input.txt')}")
+
+def test_day2_part2(capsys):
+    """
+    Test for getting the actual value for day 2 part 2
+    """
+    with capsys.disabled():
+        print(f"Answer for day 2 part 2: {get_total_score_part2('day2/input.txt')}")
+
